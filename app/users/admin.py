b1b75db22b6627d4d91fc64800ff12ea57b509d9
Rainshaw <rxg@live.com>
2021-03-01 00:13:01 +0800

    feat: can assign client model object permission in admin pages

--- app/users/admin.py
+++ app/users/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as AbstractUserAdmin
 from django.utils.translation import gettext_lazy as _
+from guardian.admin import GuardedModelAdmin
+from oidc_provider.admin import ClientForm
+from oidc_provider.models import Client
 
 from .models import User, TopLink, Announcement
 
@@ -52,3 +55,31 @@ class AnnouncementAdmin(admin.ModelAdmin):
         "content",
         "rank"
     ]
+
+
+admin.site.unregister(Client)
+
+
+@admin.register(Client)
+class ClientAdmin(GuardedModelAdmin):
+    fieldsets = [
+        [_(u''), {
+            'fields': (
+                'name', 'owner', 'client_type', 'response_types', '_redirect_uris', 'jwt_alg',
+                'require_consent', 'reuse_consent'),
+        }],
+        [_(u'Credentials'), {
+            'fields': ('client_id', 'client_secret', '_scope'),
+        }],
+        [_(u'Information'), {
+            'fields': ('contact_email', 'website_url', 'terms_url', 'logo', 'date_created'),
+        }],
+        [_(u'Session Management'), {
+            'fields': ('_post_logout_redirect_uris',),
+        }],
+    ]
+    form = ClientForm
+    list_display = ['name', 'client_id', 'response_type_descriptions', 'date_created']
+    readonly_fields = ['date_created']
+    search_fields = ['name']
+    raw_id_fields = ['owner']
