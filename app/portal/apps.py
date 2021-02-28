1b0a4bfa775cc97bc2dc11050b1e5acec08199f8
Rainshaw <rxg@live.com>
2021-03-01 04:32:34 +0800

    feat: add portal app verbose_name trans

--- app/portal/apps.py
+++ app/portal/apps.py
@@ -1,5 +1,7 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class PortalConfig(AppConfig):
     name = 'app.portal'
+    verbose_name = _('portal management')
--- app/portal/models.py
+++ app/portal/models.py
@@ -2,9 +2,6 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 
-# Create your models here.
-
-
 class TopLink(models.Model):
     name = models.CharField(
         _('Link Name'),
