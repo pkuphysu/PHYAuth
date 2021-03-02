eb273ca4fc1c9b694bc0177f4d757ae6a0f50b8b
Rainshaw <rxg@live.com>
2021-03-03 00:43:59 +0800

    feat: use user's preferred email to get gravatar

--- app/users/templatetags/user_helper.py
+++ app/users/templatetags/user_helper.py
@@ -12,7 +12,7 @@ def avatar_url(context, size=None, user=None):
     user = context['request'].user if user is None else user
     # return "/static/admin-lte/dist/img/user3-128x128.jpg"
     return 'https://cdn.v2ex.com/gravatar/{hash}?s={size}&d=mm'.format(
-        hash=md5(user.email.encode('utf-8')).hexdigest() if user.is_authenticated else '',
+        hash=md5(user.get_preferred_email().encode('utf-8')).hexdigest() if user.is_authenticated else '',
         size=size or '',
     )
 
