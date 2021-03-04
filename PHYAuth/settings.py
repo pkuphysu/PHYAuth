4b841246aee1f58b77588fa209ac462e8195396d
Rainshaw <rxg@live.com>
2021-03-04 22:32:17 +0800

    feat: add some settings for production

--- PHYAuth/settings.py
+++ PHYAuth/settings.py
@@ -10,9 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
-from pathlib import Path
-from configparser import RawConfigParser
 import sys
+from configparser import RawConfigParser
+from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -50,6 +50,9 @@ THIRD_PARTY_APPS = [
     'django_bootstrap_breadcrumbs',
     'guardian',
 ]
+if DEBUG:
+    THIRD_PARTY_APPS.append('debug_toolbar')
+    INTERNAL_IPS = ['192.168.255.198']
 
 LOCAL_APPS = [
     'app.users.apps.UsersConfig',
@@ -69,6 +72,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+if DEBUG:
+    MIDDLEWARE.insert(0, 'debug_toolbar.middleware.DebugToolbarMiddleware')
 
 ROOT_URLCONF = 'PHYAuth.urls'
 
@@ -130,22 +135,29 @@ USE_TZ = True
 
 DOMAIN = CONFIG.get("DJANGO", "DOMAIN")
 SUBPATH = CONFIG.get("DJANGO", "SUBPATH")
+if SUBPATH != '/' and not SUBPATH.endswith('/'):
+    SUBPATH += '/'
 
 LOGIN_REDIRECT_URL = SUBPATH
 LOGIN_URL = SUBPATH + 'login'
 LOGOUT_REDIRECT_URL = SUBPATH
 
-SESSION_COOKIE_PATH = LANGUAGE_COOKIE_PATH = CSRF_COOKIE_PATH = SUBPATH
+LANGUAGE_COOKIE_PATH = SUBPATH
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'assets'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-    './static/'
-]
+if DEBUG:
+    STATICFILES_DIRS = [
+        BASE_DIR / 'static',
+        './static/'
+    ]
+else:
+    STATICFILES_DIRS = [
+        BASE_DIR / 'static',
+    ]
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
@@ -206,17 +218,26 @@ NEVER_REDIS_TIMEOUT = 365 * 24 * 60 * 60
 SESSION_COOKIE_AGE = 86400
 SESSION_ENGINE = "django.contrib.sessions.backends.cache"
 SESSION_CACHE_ALIAS = "default"
+SESSION_COOKIE_PATH = SUBPATH
+SESSION_COOKIE_SECURE = True
+
+CSRF_COOKIE_SECURE = True
+CSRF_COOKIE_PATH = SUBPATH
 
 OIDC_USERINFO = 'app.users.oidc_scope_claims.userinfo'
 OIDC_EXTRA_SCOPE_CLAIMS = 'app.users.oidc_scope_claims.CustomScopeClaims'
 
+ADMINS = CONFIG.get('DJANGO', 'ADMINS')
+ADMINS = [tuple(ADMINS.split())]
+
+EMAIL_SUBJECT_PREFIX = CONFIG.get('EMAIL', 'SUBJECT_PREFIX')
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_SSL = CONFIG.getboolean('EMAIL', 'USE_SSL')
 EMAIL_HOST = CONFIG.get('EMAIL', 'HOST')
 EMAIL_PORT = CONFIG.getint('EMAIL', 'PORT')
 EMAIL_HOST_USER = CONFIG.get('EMAIL', 'USER')
 EMAIL_HOST_PASSWORD = CONFIG.get('EMAIL', 'PASSWORD')
-EMAIL_FROM = CONFIG.get('EMAIL', 'FROM')
+SERVER_EMAIL = EMAIL_FROM = CONFIG.get('EMAIL', 'FROM')
 if DEBUG:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
@@ -255,3 +276,61 @@ CELERY_TASK_SERIALIZER = "json"
 CELERY_RESULT_SERIALIZER = "json"
 CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
 CELERY_WORKER_MAX_TASKS_PER_CHILD = 100000  # 每个worker执行10w个任务就会被销毁，可防止内存泄露
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'root': {
+        'level': 'INFO',
+        'handlers': ['console'],
+    },
+    'formatters': {
+        'verbose': {
+            'format': '{levelname} {asctime} {module} {process:d} {thread:d} {message}',
+            'style': '{',
+        },
+        'simple': {
+            'format': '{levelname} {message}',
+            'style': '{',
+        },
+    },
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse',
+        },
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue',
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        },
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler',
+        },
+        'null': {
+            'class': 'logging.NullHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'propagate': True,
+        },
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+        'django.server': {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+    }
+}
--- PHYAuth/urls.py
+++ PHYAuth/urls.py
@@ -13,8 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+
 from app.portal import views as portal_views
 
 urlpatterns = [
@@ -46,3 +48,10 @@ urlpatterns = [
     # admin/
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns += [
+        path('__debug__/', include(debug_toolbar.urls))
+    ]
