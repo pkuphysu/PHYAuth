f078a7934edb4671250235e7a31b498ebf6e142f
Rainshaw <rxg@live.com>
2021-03-05 01:11:32 +0800

    fix: client manage page permission

--- app/users/views.py
+++ app/users/views.py
@@ -16,7 +16,6 @@ class UserProfileView(ObjectPermissionRequiredMixin, SuccessMessageMixin, Update
     template_name = 'users/user_detail.html'
     form_class = UserForm
     permission_required = ['users.change_user', 'users.view_user']
-    raise_exception = True
     success_message = _('Your profile has been updated successfully!')
 
     def get_context_data(self, **kwargs):
@@ -37,7 +36,6 @@ class ClientListView(PermissionRequiredMixin, PermissionListMixin, ListView):
     model = Client
     template_name = 'users/client_list.html'
     permission_required = 'oidc_provider.view_client'
-    raise_exception = True
     permission_denied_message = _('You are not a developer and do not have permission to view the application, '
                                   'please contact the administrator!')
     context_object_name = 'client_list'
@@ -50,7 +48,6 @@ class ClientCreateView(PermissionRequiredMixin, ObjectPermissionRequiredMixin, S
     template_name = 'users/client_create.html'
     permission_object = None
     permission_required = 'oidc_provider.add_client'
-    raise_exception = True
     permission_denied_message = _('You are not a developer and do not have permission to view the application, '
                                   'please contact the administrator!')
     success_message = _('Client %(name)s has been created successfully!')
@@ -72,8 +69,8 @@ class ClientUpdateView(PermissionRequiredMixin, ObjectPermissionRequiredMixin, S
     permission_required = 'oidc_provider.change_client'
     permission_denied_message = _('You are not the creator of this application, '
                                   'so you do not have the right to modify this application!')
-    raise_exception = True
     success_message = _('Client %(name)s has been updated successfully!')
+    return_403 = True
 
     def get_success_url(self):
         return reverse('users:client-list')
