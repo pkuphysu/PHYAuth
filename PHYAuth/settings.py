451b4546deea5af86e2b7aebe69b003ecdadcec6
Rainshaw <rxg@live.com>
2021-03-12 01:21:15 +0800

    feat: show debug tool bar callback

--- PHYAuth/settings.py
+++ PHYAuth/settings.py
@@ -52,7 +52,30 @@ THIRD_PARTY_APPS = [
 ]
 if DEBUG:
     THIRD_PARTY_APPS.append('debug_toolbar')
-    INTERNAL_IPS = ['192.168.255.198']
+    DEBUG_TOOLBAR_PANELS = [
+        'debug_toolbar.panels.history.HistoryPanel',
+        'debug_toolbar.panels.timer.TimerPanel',
+        'debug_toolbar.panels.settings.SettingsPanel',
+        'debug_toolbar.panels.headers.HeadersPanel',
+        'debug_toolbar.panels.request.RequestPanel',
+        'debug_toolbar.panels.sql.SQLPanel',
+        'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+        'debug_toolbar.panels.templates.TemplatesPanel',
+        'debug_toolbar.panels.cache.CachePanel',
+        'debug_toolbar.panels.signals.SignalsPanel',
+        'debug_toolbar.panels.logging.LoggingPanel',
+        'debug_toolbar.panels.redirects.RedirectsPanel',
+        'debug_toolbar.panels.profiling.ProfilingPanel',
+    ]
+
+
+    def show_toolbar_callback(request):
+        return True
+
+
+    DEBUG_TOOLBAR_CONFIG = {
+        'SHOW_TOOLBAR_CALLBACK': show_toolbar_callback
+    }
 
 LOCAL_APPS = [
     'app.users.apps.UsersConfig',
