58f9bbac3ae436f33cf1475f8c8c09ad614fb27c
Rainshaw <rxg@live.com>
2021-03-15 22:41:15 +0800

    feat: add cache for announcements in index.html

--- app/portal/signals.py
+++ app/portal/signals.py
@@ -5,7 +5,7 @@ from django.core.cache.utils import make_template_fragment_key
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 
-from .models import TopLink
+from .models import TopLink, Announcement
 
 logger = logging.getLogger(__name__)
 
@@ -16,3 +16,11 @@ def toplink_update(sender, **kwargs):
     key = make_template_fragment_key('nav_top_links')
     cache.delete(key)
     logger.info(f'Delete nav_top_links cache.')
+
+
+@receiver(post_delete, sender=Announcement)
+@receiver(post_save, sender=Announcement)
+def toplink_update(sender, **kwargs):
+    key = make_template_fragment_key('index_announcements')
+    cache.delete(key)
+    logger.info(f'Delete index_announcements cache.')
