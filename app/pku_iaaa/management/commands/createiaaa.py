c8b0b96206445b60ef2bfde8f95e63a7b08f514d
Rainshaw <rxg@live.com>
2021-03-19 03:31:15 +0800

    feat: add create iaaa app command

---
+++ app/pku_iaaa/management/commands/createiaaa.py
@@ -0,0 +1,24 @@
+from django.core.management.base import BaseCommand
+from ...models import Iaaa
+
+
+class Command(BaseCommand):
+    help = 'Create iaaa app info.'
+
+    def handle(self, *args, **options):
+        try:
+            app_id = input('Please input the appID: ')
+            if app_id is '':
+                raise ValueError('app id can not be empty!')
+            key = input('Please input the appKEY: ')
+            if key is '':
+                raise ValueError('app key can not be empty!')
+            redirect_url = input('Please inputh the redirect_url: ')
+            if redirect_url is '':
+                raise ValueError('redirect_url can not be empty!')
+            if not redirect_url.startswith('http'):
+                raise ValueError('redirect_url should start with http!')
+            Iaaa.objects.create(app_id=app_id, key=key, redirect_url=redirect_url)
+            self.stdout.write(self.style.SUCCESS('IAAA app successfully created.'))
+        except Exception as e:
+            self.stdout.write(self.style.ERROR('Something goes wrong: {0}'.format(e)))
