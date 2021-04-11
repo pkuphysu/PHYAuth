74e5921072a68b42c31dff50dddeadcaf142b3a9
Rainshaw <rxg@live.com>
2021-04-12 01:56:12 +0800

    fix: autofield in django3.2 warnings

--- PHYAuth/settings.py
+++ PHYAuth/settings.py
@@ -176,6 +176,11 @@ LANGUAGES = [
     ('en', 'English'),
 ]
 
+# Default primary key field type
+# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
